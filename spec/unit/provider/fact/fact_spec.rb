6b4f11e8bad7cb19b729f53647a6fcda82b5494d
Vikraman Choudhury <vikraman@gentoo.org>
2015-01-31 20:42:45 +0100

    Add RSpec tests for the fact type and provider

---
+++ spec/unit/provider/fact/fact_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:fact).provider(:fact) do
+  it "should remove empty files" do
+    described_class.unlink_empty_files == true
+  end
+
+  it "should have a default target of /etc/facter/facts.d/$name.txt" do
+    described_class.new(:name => "environment").select_file == "/etc/facter/facts.d/enironment.txt"
+  end
+
+  it "should have a target of /etc/facter/facts.d/$target.txt when target is specified" do
+    described_class.new(:name => "environment", :target => "env").select_file == "/etc/facter/facts.d/env.txt"
+  end
+
+  context "when parsing" do
+    props = described_class.parse_file("/etc/facter/facts.d/env.txt", "environment=production")
+    prop = props.find { |h| h[:name] == "environment" }
+
+    it "should detect name correctly" do
+      prop.should_not be_nil
+    end
+
+    it "should detect content correctly" do
+      prop.should include(:content => "production")
+    end
+
+    it "should detect target correctly" do
+      prop.should include(:target => "env")
+    end
+  end
+
+  context "when formatting" do
+    let(:environment_provider) do
+      stub("environment_provider",
+           :name => "environment",
+           :ensure => :present,
+           :content => "production",
+           :target => "env"
+          )
+    end
+
+    let(:environment_content) { described_class.format_file("/etc/facter/facts.d/env.txt", [environment_provider]) }
+
+    it "should write content correctly" do
+      environment_content.should == "environment=production\n"
+    end
+
+    let(:absent_provider) do
+      stub("absent_provider",
+           :name => "environment",
+           :ensure => :absent,
+           :content => "production",
+           :target => "env"
+          )
+    end
+
+    it "should cleanup absent resources" do
+      described_class.format_file("/etc/facter/facts.d/env.txt", [absent_provider]).should be_empty
+    end
+
+
+    let(:nonexistent_provider) do
+      stub("nonexistent_provider",
+           :name => "environment",
+           :ensure => :absent,
+           :content => "production",
+           :target => "environment"
+          )
+    end
+
+    it "should cleanup nonexistent resources" do
+      described_class.format_file("/etc/facter/facts.d/env.txt", [nonexistent_provider]).should be_empty
+    end
+  end
+end
---
+++ spec/unit/type/fact_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe Puppet::Type.type(:fact) do
+  context "when validating attributes" do
+    params = [:name]
+    properties = [:content, :target]
+
+    params.each do |param|
+      it "should have the #{param} param" do
+        described_class.attrtype(param).should == :param
+      end
+    end
+
+    properties.each do |property|
+      it "should have the #{property} property" do
+        described_class.attrtype(property).should == :property
+      end
+    end
+  end
+
+  context "when validating content" do
+    it "should reject only whitespace" do
+      expect { described_class.new(:name => "environment", :content => " ") }.to raise_error
+    end
+
+    it "should accept other values" do
+      expect { described_class.new(:name => "environment", :content => "production") }.to_not raise_error
+      expect { described_class.new(:name => "environment", :content => " production") }.to_not raise_error
+    end
+  end
+
+  context "when validating target" do
+    resource = described_class.new(:name => "environment")
+
+    it "can be optional" do
+      expect { resource }.to_not raise_error
+    end
+
+    it "should have :name as default value" do
+      resource.should(:target).should == resource[:name]
+    end
+
+    it "should accept specified values" do
+      described_class.new(:name => "environment", :target => "env").should(:target).should == "env"
+    end
+  end
+
+  it "should have name as the namevar" do
+    described_class.key_attributes.should == [:name]
+  end
+end
